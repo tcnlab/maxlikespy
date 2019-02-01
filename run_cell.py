564e9f1122760a04061c1c59909775d3fa87ea24
Steve Charczynski <stevecharczynski@Steves-MacBook-Pro.local>
2019-02-01 18:07:15 -0500

    fixing comment

--- run_cell.py
+++ run_cell.py
@@ -61,11 +61,10 @@ def run_script(cell_range):
 
     # cell_range[1] += 1
     
-    # util.collect_data(cell_range, "log_likelihoods")
-    # util.collect_data(cell_range, "model_comparisons")
-    # util.collect_data(cell_range, "cell_fits")
+    util.collect_data(cell_range, "log_likelihoods")
+    util.collect_data(cell_range, "model_comparisons")
+    util.collect_data(cell_range, "cell_fits")
 
-run_script([44,45])
 if __name__ == "__main__":
     cell_range = sys.argv[-2:]
     cell_range = list(map(int, cell_range))
