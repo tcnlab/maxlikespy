780a2ee92d6f5e4e0b11d40f6a00e6592093d74a
scharczynski <scharcz@brandeis.edu>
2019-04-29 15:38:48 -0400

    updated script example

--- examples/run_script.py
+++ examples/run_script.py
@@ -1,16 +1,14 @@
 import sys
-import maxlikespy.data_processor as dp
-import maxlikespy.analysis_pipeline as ap
+import maxlikespy.analysis as analysis
 import json
 import os
-import maxlikespy.util as util
 import numpy as np
 
 
 def run_script(cell_range):
 
     path_to_data = '/Users/stevecharczynski/workspace/data/jay/2nd_file'
-    data_processor = dp.DataProcessor(path_to_data, cell_range)
+    data_processor = analysis.DataProcessor(path_to_data, cell_range)
     solver_params = {
         "niter": 2,
         "stepsize": 10000,
@@ -19,23 +17,23 @@ def run_script(cell_range):
         "use_jac": True,
     }
     bounds_t = {
-        "a_1": [0, 1 / n],
+        "a_1": [0, 1 / 2],
         "ut": [-1000,10000],
         "st": [100, 10000],
-        "a_0": [10**-10, 1 / n]
+        "a_0": [10**-10, 1 / 2]
     }
     bounds_c = {
-        "a_0": [10**-10, 1 / n]
+        "a_0": [10**-10, 1 / 2]
     }
-    pipeline = ap.AnalysisPipeline(cell_range, data_processor, [
+    pipeline = analysis.AnalysisPipeline(cell_range, data_processor, [
                                 "Const", "Time"], 0)
     pipeline.set_model_bounds("Time", bounds_t)
     pipeline.set_model_bounds("Const", bounds_c)
     pipeline.set_model_x0(["Const", "Time"], [[1e-5], [1e-5, 100, 100, 1e-5]])
     pipeline.fit_even_odd(solver_params)
+    pipeline.compare_even_odd("Const", "Time", 0.01)
     pipeline.fit_all_models(solver_params=solver_params)
     pipeline.compare_models("Const", "Time", 0.01)
-
 
 if __name__ == "__main__":
     cell_range = sys.argv[-2:]
